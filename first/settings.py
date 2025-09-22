af2f8a4a1c799105b20b445b73a6ec3c93143d67
Dbhosale146 <75349936+Dbhosale146@users.noreply.github.com>
2025-09-22 09:48:05 +0530

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -78,10 +78,19 @@ WSGI_APPLICATION = 'first.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',     
+        'NAME': 'sys_db',
+        'USER': 'root',
+        'PASSWORD': 'Pass@1234',
+        'HOST': 'localhost',                      
+        'PORT': '3306',                           
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
+
 '''
 DATABASES = {
     'default': {
